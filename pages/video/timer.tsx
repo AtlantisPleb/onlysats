db24ac41b463a2670f8d20d9865046d181e883f5
Christopher David <chris@arcade.city>
2021-08-22 12:46:30 -0500

    algorithm for charging sats per interval

--- pages/video/timer.tsx
+++ pages/video/timer.tsx
@@ -1,14 +1,21 @@
 import { useEffect } from 'react'
+import { useStore } from '@/helpers/store'
 import { useElapsedTime } from 'use-elapsed-time'
 
-const interval = 60
-let chargedFirstSat = false
+const amount = 5 // sats to charge every interval
+const interval = 3 // interval in seconds
+const charged: any = []
 
 export const Timer = ({ isPlaying }: any) => {
+  const wallet = useStore((s) => s.wallet)
   const onUpdate = (time: number) => {
-    if (time > 10 && !chargedFirstSat) {
-      console.log('CHARGING 1 SAT')
-      chargedFirstSat = true
+    const times = time / interval
+
+    // compare times we should have to how many times we did
+    if (times > charged.length) {
+      charged.push('demo charge')
+      // wallet.chargeVideoView(amount)
+      console.log('Charged length is now:', charged.length)
     }
   }
   const { elapsedTime } = useElapsedTime({ isPlaying, onUpdate })
