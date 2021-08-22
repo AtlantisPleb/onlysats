e4196ba68648e8d648ea21148451ab8ad57fdf3a
Christopher David <chris@arcade.city>
2021-08-21 19:45:48 -0500

    Axe more

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 // https://blog.tailwindcss.com/building-the-tailwind-blog
-const { createLoader } = require('simple-functional-loader')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-const withSyntaxHighlighting = require('./remark/withSyntaxHighlighting')
-const withProse = require('./remark/withProse')
 
 module.exports = withBundleAnalyzer({
   // reactStrictMode: true,
@@ -36,70 +33,6 @@ module.exports = withBundleAnalyzer({
             publicPath: '/_next',
             name: 'static/media/[name].[hash].[ext]',
           },
-        },
-      ],
-    })
-
-    const mdx = [
-      options.defaultLoaders.babel,
-      {
-        loader: '@mdx-js/loader',
-        options: {
-          remarkPlugins: [withProse, withSyntaxHighlighting],
-        },
-      },
-    ]
-
-    config.module.rules.push({
-      test: /\.mdx$/,
-      oneOf: [
-        {
-          test: /snippets/,
-          use: mdx,
-        },
-        {
-          resourceQuery: /preview/,
-          use: [
-            ...mdx,
-            createLoader(function (src) {
-              if (src.includes('<!--more-->')) {
-                const [preview] = src.split('<!--more-->')
-                return this.callback(null, preview)
-              }
-
-              const [preview] = src.split('<!--/excerpt-->')
-              return this.callback(null, preview.replace('<!--excerpt-->', ''))
-            }),
-          ],
-        },
-        {
-          resourceQuery: /rss/,
-          use: mdx,
-        },
-        {
-          use: [
-            ...mdx,
-            createLoader(function (src) {
-              const content = [
-                'import Post from "@/components/Post"',
-                'export { getStaticProps } from "@/getStaticProps"',
-                src,
-                'export default Post',
-              ].join('\n')
-
-              if (content.includes('<!--more-->')) {
-                return this.callback(
-                  null,
-                  content.split('<!--more-->').join('\n')
-                )
-              }
-
-              return this.callback(
-                null,
-                content.replace(/<!--excerpt-->.*<!--\/excerpt-->/s, '')
-              )
-            }),
-          ],
         },
       ],
     })
