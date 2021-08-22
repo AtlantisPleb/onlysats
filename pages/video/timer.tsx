d93b9b8648977a018dca9b1e88ed0e659e990d24
Christopher David <chris@arcade.city>
2021-08-22 12:23:47 -0500

    Initial determination of charging first sat - after 10s

--- pages/video/timer.tsx
+++ pages/video/timer.tsx
@@ -1,8 +1,17 @@
 import { useEffect } from 'react'
 import { useElapsedTime } from 'use-elapsed-time'
 
+const interval = 60
+let chargedFirstSat = false
+
 export const Timer = ({ isPlaying }: any) => {
-  const { elapsedTime } = useElapsedTime({ isPlaying })
+  const onUpdate = (time: number) => {
+    if (time > 10 && !chargedFirstSat) {
+      console.log('CHARGING 1 SAT')
+      chargedFirstSat = true
+    }
+  }
+  const { elapsedTime } = useElapsedTime({ isPlaying, onUpdate })
   useEffect(() => {
     console.log(isPlaying, elapsedTime)
   }, [isPlaying])
