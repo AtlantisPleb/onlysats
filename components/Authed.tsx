427c7b3106cd4cbd8e342c75d6fd30f568dd6013
Christopher David <chris@arcade.city>
2021-08-22 01:47:49 -0500

    Add vimeo player

--- components/Authed.tsx
+++ components/Authed.tsx
@@ -1,3 +1,29 @@
+import { useEffect, useState } from 'react'
+import { View } from 'react-native'
+// import ReactPlayer from 'react-player'
+// @ts-ignore
+import Player from '@vimeo/player'
+
 export const Authed = () => {
-  return <></>
+  const [isPlaying, setIsPlaying] = useState(true)
+  useEffect(() => {
+    const player = new Player('handstick', {
+      id: 590491522,
+      width: 640,
+      height: 900,
+      controls: true,
+      title: false,
+      byline: false,
+      portrait: false,
+    })
+    player.on('play', function () {
+      console.log('played the video!')
+    })
+  }, [])
+  return (
+    <View style={{ width: 640, height: 900, backgroundColor: 'transparent' }}>
+      <div id='handstick' />
+      {/* <ReactPlayer url='https://vimeo.com/590468811' playing={isPlaying} /> */}
+    </View>
+  )
 }
