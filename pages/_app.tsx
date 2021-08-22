8d7c17a94b4bd9352061a9fe8f65f82cb91abe52
Christopher David <chris@arcade.city>
2021-08-21 22:03:45 -0500

    Load LNPay script async

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,7 +19,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <script src='/lnpay.min.js'></script>
+        <script async src='/lnpay.min.js'></script>
       </Head>
       <Header />
       <LCanvas />
